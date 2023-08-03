ec665051b73971df69bb639988a334f8780ca79f
Daniel Muñoz <danielmunozmoreno2005@gmail.com>
2023-08-03 00:21:42 -0500

    octavo ejercicio

---
+++ 8_Ejercicio.py
@@ -0,0 +1,7 @@
+def invertir_lista(lista):
+    return lista[::-1]
+
+lista_original = [3, 12, 89, 4, 32]
+lista_invertida = invertir_lista(lista_original)
+print("Lista original:", lista_original)
+print("Lista invertida:", lista_invertida)
