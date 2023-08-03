baae99058853ea182a3c74f2fa4c7da351f3b9b0
Daniel Muñoz <danielmunozmoreno2005@gmail.com>
2023-08-03 00:07:32 -0500

    5 ejercicio

---
+++ 5_Ejercicio.py
@@ -0,0 +1,10 @@
+def fahrenheit_a_celsius(temperatura):
+    return (temperatura - 32) / 1.8
+
+n = float(input("ingresa la temperatura: "))
+
+temperatura_c = fahrenheit_a_celsius(n)
+print(f'La temperatura en ºC es: {temperatura_c}')
+
+temperatura_c = fahrenheit_a_celsius(n)
+print(f'La temperatura en ºC es: {temperatura_c}')
